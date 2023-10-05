86987fc024b576fedf4af3d936e0be376c86f736
thamarai866 <SM2078@solverminds.com>
2023-10-05 11:52:10 +0530

    ff

---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+
+provider "aws" {
+  region                  = "${var.region}"
+  access_key = "AKIAUFJG6ALWUBGS5T4H"
+  secret_key = "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"
+  # shared_credentials_file = "~/.aws/credentials"
+  # profile                 = "development"
+}
+
